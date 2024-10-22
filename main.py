5fa025680e846ed0bd2b4e8797b1df8813b3f0e4
Fvesta <wt0630@naver.com>
2024-10-22 22:36:16 +0900

    Add exception hook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import traceback
 
 # Change resource path
 try:
@@ -13,6 +14,7 @@ from PySide2.QtAxContainer import QAxWidget
 
 app = QApplication(sys.argv)
 
+from core.logger import logger
 from core.api import API
 from core.callback_handler import CallbackHandler
 from core.real_processing import real_manager
@@ -21,8 +23,25 @@ from core.condition import cond_manager
 from core.kiwoom import Kiwoom
 from windows.main_win.main_win import MainWin
 
+def exception_hook(exctype, value, tb):
+    error_message = ''.join(traceback.format_exception(exctype, value, tb))
+    logger.error(error_message)
+    
+    # Set popup message
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Critical)
+    msg.setText("예외가 발생했습니다!")
+    msg.setInformativeText(error_message)
+    msg.setWindowTitle("Error")
+    msg.exec_()
+
+    # Not exit
+    # sys.exit(1)  # 주석 처리: 프로그램 종료 방지
+
 if __name__ == "__main__":
     ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+    
+    sys.excepthook = exception_hook
 
     kiwoom = Kiwoom(ocx)
     api = API(kiwoom)
